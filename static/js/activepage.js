592ee2d3a70ee316c5603cca2662cd215dc8a16c
Shashwat <shashwatvarenya@gmail.com>
2023-06-06 23:28:46 +0530

    customized pages

--- static/js/activepage.js
+++
@@ -1,8 +0,0 @@
-const activepage = window.location.pathname;
-console.log(window.location)
-const navlinks = document.querySelectorAll('.navbar').forEach(link => {
-
-    if(link.href.include('${activepage}')){
-        link.classList.add('active')
-    }
-})
