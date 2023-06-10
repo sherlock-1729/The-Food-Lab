21f4b294acfd6cd423934b9e1933f65ade514b84
sherlock1729 <shashwatvarenya@gmail.com>
2023-06-10 13:43:17 +0530

    added brands

--- static/js/script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#toggle-bar');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
-}
